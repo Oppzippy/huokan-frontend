e5aa0cb63440995c16146422f832ed929dfe8578
Kyle Scheuing <kyle@kylescheuing.com>
2022-02-02 02:37:07 -0500

    Add pagination to deposits

--- src/components/pages/DepositsPage.svelte
+++ src/components/pages/DepositsPage.svelte
@@ -1,26 +1,98 @@
 <script lang="ts">
 	import type { Deposit } from "@huokan/huokan-api-client";
 
-	import { DataTable } from "carbon-components-svelte";
+	import {
+		Button,
+		DataTable,
+		DataTableSkeleton,
+	} from "carbon-components-svelte";
 	import { authenticatedRepositoriesStore } from "../../stores/current-user/AuthenticatedRepositoriesStore";
 	import { selectedOrganizationStore } from "../../stores/current-user/OrganizationsStore";
 	import GuildSelection from "../inputs/GuildSelection.svelte";
 	import Gold from "../text/Gold.svelte";
 
 	let selectedGuildId: string | undefined;
+	let pageLimit = 25;
 
-	let depositsPromise: Promise<Deposit[]> | null;
+	let deposits: Deposit[] | null;
 	$: if (
 		$authenticatedRepositoriesStore != null &&
 		$selectedOrganizationStore != null &&
 		selectedGuildId != null
 	) {
-		depositsPromise =
-			$authenticatedRepositoriesStore.depositRepository.getDeposits(
+		$authenticatedRepositoriesStore.depositRepository
+			.getDeposits(
 				$selectedOrganizationStore.id,
-				selectedGuildId
-			);
+				selectedGuildId,
+				pageLimit
+			)
+			.then((newDeposits) => (deposits = newDeposits));
 	}
+
+	function showNewerPage() {
+		if (
+			$authenticatedRepositoriesStore != null &&
+			$selectedOrganizationStore != null &&
+			selectedGuildId != null &&
+			deposits != null &&
+			deposits.length >= 1
+		) {
+			const oldDeposits = deposits;
+			$authenticatedRepositoriesStore?.depositRepository
+				.getNewerPage(
+					$selectedOrganizationStore.id,
+					selectedGuildId,
+					deposits[deposits.length - 1].id,
+					pageLimit
+				)
+				.then((newDeposits) => {
+					if (newDeposits.length < pageLimit) {
+						newDeposits = [
+							...oldDeposits.slice(
+								newDeposits.length - pageLimit,
+								oldDeposits.length
+							),
+							...newDeposits,
+						];
+					}
+					deposits = newDeposits;
+				});
+		}
+	}
+
+	function showOlderPage() {
+		if (
+			$authenticatedRepositoriesStore != null &&
+			$selectedOrganizationStore != null &&
+			selectedGuildId != null &&
+			deposits != null &&
+			deposits.length >= 1
+		) {
+			$authenticatedRepositoriesStore?.depositRepository
+				.getOlderPage(
+					$selectedOrganizationStore.id,
+					selectedGuildId,
+					deposits[0].id,
+					pageLimit
+				)
+				.then((newDeposits) => {
+					if (newDeposits.length > 0) {
+						deposits = newDeposits;
+					}
+				});
+		}
+	}
+
+	const dataTableHeaders = [
+		{ key: "id", value: "ID" },
+		{ key: "characterName", value: "Character Name" },
+		{ key: "money", value: "Deposit" },
+		{ key: "endorsements", value: "Endorsements" },
+		{
+			key: "approximateDepositTimestamp",
+			value: "Approximate Deposit Time",
+		},
+	];
 </script>
 
 {#if $selectedOrganizationStore != null}
@@ -28,26 +100,25 @@
 		organizationId="{$selectedOrganizationStore.id}"
 		bind:selectedGuildId
 	/>
-	{#await depositsPromise}
-		Loading deposits...
-	{:then deposits}
-		{#if deposits != null}
+	{#if selectedGuildId != null}
+		{#if deposits == null}
+			<DataTableSkeleton
+				headers="{dataTableHeaders}"
+				rows="{pageLimit}"
+			/>
+		{:else if deposits != null}
+			<Button on:click="{showNewerPage}">Newer</Button>
+			<Button on:click="{showOlderPage}">Older</Button>
 			<DataTable
-				headers="{[
-					{ key: 'characterName', value: 'Character Name' },
-					{ key: 'money', value: 'Deposit' },
-					{ key: 'endorsements', value: 'Endorsements' },
-					{
-						key: 'approximateDepositTimestamp',
-						value: 'Approximate Deposit Time',
-					},
-				]}"
-				rows="{deposits.map((deposit) => ({
-					...deposit,
-					money: deposit.depositInCopper,
-					approximateDepositTimestamp:
-						deposit.approximateDepositTimestamp.toLocaleString(),
-				}))}"
+				headers="{dataTableHeaders}"
+				rows="{deposits
+					.map((deposit) => ({
+						...deposit,
+						money: deposit.depositInCopper,
+						approximateDepositTimestamp:
+							deposit.approximateDepositTimestamp.toLocaleString(),
+					}))
+					.reverse()}"
 			>
 				<svelte:fragment slot="cell" let:cell>
 					{#if cell.key == "money"}
@@ -57,10 +128,8 @@
 					{/if}
 				</svelte:fragment>
 			</DataTable>
-		{:else}
-			No guild selected.
 		{/if}
-	{/await}
+	{/if}
 {:else}
 	No organization is selected.
 {/if}
--- src/data-access/huokan/DepositRepository.ts
+++ src/data-access/huokan/DepositRepository.ts
@@ -11,11 +11,49 @@ export class DepositRepository {
 
 	public async getDeposits(
 		organizationId: string,
-		guildId: string
+		guildId: string,
+		pageLimit: number
 	): Promise<Deposit[]> {
 		const { deposits } = await this.api.getDeposits({
 			organizationId,
 			guildId,
+			limit: pageLimit,
+		});
+		return deposits;
+	}
+
+	public async getNewerPage(
+		organizationId: string,
+		guildId: string,
+		newestDepositIdOnCurrentPage: string,
+		pageLimit: number
+	): Promise<Deposit[]> {
+		const { deposits } = await this.api.getDeposits({
+			organizationId,
+			guildId,
+			after: newestDepositIdOnCurrentPage,
+			limit: pageLimit,
+		});
+		return deposits;
+	}
+
+	public async getOlderPage(
+		organizationId: string,
+		guildId: string,
+		oldestDepositIdOnCurrentPage: string,
+		pageLimit: number
+	): Promise<Deposit[]> {
+		const olderPageStart = await this.api.getDeposit({
+			organizationId,
+			guildId,
+			depositId: oldestDepositIdOnCurrentPage,
+			offset: -pageLimit - 1,
+		});
+		const { deposits } = await this.api.getDeposits({
+			organizationId,
+			guildId,
+			after: olderPageStart.id,
+			limit: pageLimit,
 		});
 		return deposits;
 	}
