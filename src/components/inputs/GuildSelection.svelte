e87ecc74c95cf8d496713f16c964be3728368660
Kyle Scheuing <kyle@kylescheuing.com>
2022-02-02 02:36:25 -0500

    Use a ComboBox instead of Select in GuildSelection

--- src/components/inputs/GuildSelection.svelte
+++ src/components/inputs/GuildSelection.svelte
@@ -1,37 +1,33 @@
 <script lang="ts">
 	import type { Guild } from "@huokan/huokan-api-client";
 
-	import {
-		Select,
-		SelectItem,
-		SelectSkeleton,
-	} from "carbon-components-svelte";
+	import { ComboBox, SelectSkeleton } from "carbon-components-svelte";
 	import { authenticatedRepositoriesStore } from "../../stores/current-user/AuthenticatedRepositoriesStore";
 
 	export let organizationId: string;
 	export let selectedGuildId: string | undefined = undefined;
 
 	let guilds: Guild[] | null;
-	function setGuilds(newGuilds: Guild[]) {
-		guilds = newGuilds;
-		selectedGuildId = guilds[0]?.id;
-	}
 	$: if ($authenticatedRepositoriesStore != null) {
 		$authenticatedRepositoriesStore.guildRepository
 			.getGuilds(organizationId)
-			.then(setGuilds);
+			.then((newGuilds) => (guilds = newGuilds));
+	} else {
+		guilds = null;
 	}
 </script>
 
 {#if guilds == null}
 	<SelectSkeleton />
 {:else}
-	<Select labelText="Guild" on:change bind:selected="{selectedGuildId}">
-		{#each guilds as guild}
-			<SelectItem
-				value="{guild.id}"
-				text="{`${guild.name}-${guild.realm}`}"
-			/>
-		{/each}
-	</Select>
+	<ComboBox
+		titleText="Guild"
+		placeholder="Select guild bank to view"
+		on:select
+		bind:selectedId="{selectedGuildId}"
+		items="{guilds.map((guild) => ({
+			id: guild.id,
+			text: `${guild.name}-${guild.realm}`,
+		}))}"
+	/>
 {/if}
